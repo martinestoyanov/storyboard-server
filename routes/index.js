cec8b8e59b8633ef628e3b42e0575928b334650e
Martin Stoyanov <martinestoyanov@gmail.com>
2021-06-08 21:23:05 -0400

    api routes basics

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const authRoutes = require("./auth.routes");
+const api = require("./api.routes");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -7,5 +8,7 @@ router.get("/", (req, res, next) => {
 });
 
 router.use("/auth", authRoutes);
+router.use("/api", api);
+
 
 module.exports = router;
