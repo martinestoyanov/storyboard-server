d7f283857e5e9d5544c4ece0ec1b250daa99ca30
Derek <shouldergiant@icloud.com>
2021-06-24 18:38:16 -0400

    added specific file name to uploader.single()

--- routes/image-upload.routes.js
+++ routes/image-upload.routes.js
@@ -1,13 +1,20 @@
 const router = require("express").Router();
 
+const hasBackendAuth = require("../middleware/hasBackendAuth");
+
 const uploader = require("../config/cloudinary.config");
 
-router.post("/upload", uploader.single("imageUrl"), (req, res, next) => {
-  if (!req.file) {
-    next(new Error("No file uploaded"));
-    return;
+router.post(
+  "/upload",
+  hasBackendAuth,
+  uploader.single("pictureUrl"),
+  (req, res, next) => {
+    if (!req.file) {
+      next(new Error("No file uploaded"));
+      return;
+    }
+    res.json(200).json({ secure_url: req.file.path });
   }
-  res.json(200).json({ secure_url: req.file.path });
-});
+);
 
 module.exports = router;
